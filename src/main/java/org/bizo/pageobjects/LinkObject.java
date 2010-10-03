f90f5e799f8509280dda8fe7e6d0b74c26563924
Stephen Haberman <stephen@exigencecorp.com>
2010-10-03 16:58:31 -0600

    Add LinkObject.afterClickWaitFor.

--- src/main/java/org/bizo/pageobjects/LinkObject.java
+++ src/main/java/org/bizo/pageobjects/LinkObject.java
@@ -1,15 +1,38 @@
 package org.bizo.pageobjects;
 
+import static com.google.common.collect.Lists.newArrayList;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
+import java.util.List;
+
+import org.openqa.selenium.By;
+
+/** A helper object to click on links. */
 public class LinkObject extends AbstractElementObject {
+
+  private final List<Condition> afterClickWaitFor = newArrayList();
+
   public LinkObject(final PageObject p, final String id) {
     super(p, id);
   }
 
+  public LinkObject(final PageObject p, By by) {
+    super(p, by);
+  }
+
+  /** Adds {@code condition} as something to wait for after clicking. */
+  public LinkObject afterClickWaitFor(Condition condition) {
+    afterClickWaitFor.add(condition);
+    return this;
+  }
+
+  /** Clicks on the link and optionally waits for any after click conditions. */
   public void click() {
     element().click();
+    for (Condition condition : afterClickWaitFor) {
+      p.waitFor(condition);
+    }
   }
 
   public String getTitle() {
