8ff0fd2838f8f4f4ce0bde8b4d03df72dd95310c
Stephen Haberman <stephen@exigencecorp.com>
2010-10-03 16:57:25 -0600

    Extract ByWithIdInterpolation.

--- src/main/java/org/bizo/pageobjects/AbstractElementObject.java
+++ src/main/java/org/bizo/pageobjects/AbstractElementObject.java
@@ -3,16 +3,11 @@ package org.bizo.pageobjects;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
-import java.util.List;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.RenderedWebElement;
-import org.openqa.selenium.SearchContext;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.internal.FindsById;
-import org.openqa.selenium.internal.FindsByXPath;
 
 import com.google.common.base.Supplier;
 
@@ -26,7 +21,7 @@ public abstract class AbstractElementObject {
 
   public AbstractElementObject(final PageObject p, final String id) {
     this.p = p;
-    by = new ByWithIdInterpolation(id);
+    by = new ByWithIdInterpolation(p, id);
   }
 
   public AbstractElementObject(final PageObject p, final By by) {
@@ -95,41 +90,4 @@ public abstract class AbstractElementObject {
     };
   }
 
-  /** Replaces {@code $id} in otherwise static element ids for use in table/list scenarios. */
-  private final class ByWithIdInterpolation extends By {
-    private final String id;
-
-    private ByWithIdInterpolation(final String id) {
-      this.id = id;
-    }
-
-    @Override
-    public List<WebElement> findElements(final SearchContext context) {
-      // Copy/paste of By.id with $id/page offset id interpolation added
-      final String id = getInterpolatedId();
-      if (context instanceof FindsById) {
-        return ((FindsById) context).findElementsById(id);
-      }
-      return ((FindsByXPath) context).findElementsByXPath("*[@id = '" + id + "']");
-    }
-
-    @Override
-    public WebElement findElement(final SearchContext context) {
-      // Copy/paste of By.id with $id/page offset id interpolation added
-      final String id = getInterpolatedId();
-      if (context instanceof FindsById) {
-        return ((FindsById) context).findElementById(id);
-      }
-      return ((FindsByXPath) context).findElementByXPath("*[@id = '" + id + "']");
-    }
-
-    @Override
-    public String toString() {
-      return "By.id: " + getInterpolatedId();
-    }
-
-    private String getInterpolatedId() {
-      return p.getOffsetId() == null ? id : id.replace("$id", p.getOffsetId());
-    }
-  }
 }
---
+++ src/main/java/org/bizo/pageobjects/ByWithIdInterpolation.java
@@ -0,0 +1,49 @@
+package org.bizo.pageobjects;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.SearchContext;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.internal.FindsById;
+import org.openqa.selenium.internal.FindsByXPath;
+
+/** Replaces {@code $id} in otherwise static element ids for use in table/list scenarios. */
+public final class ByWithIdInterpolation extends By {
+  private final PageObject page;
+  private final String id;
+
+  public ByWithIdInterpolation(final PageObject page, final String id) {
+    this.page = page;
+    this.id = id;
+  }
+
+  @Override
+  public List<WebElement> findElements(final SearchContext context) {
+    // Copy/paste of By.id with $id/page offset id interpolation added
+    final String id = getInterpolatedId();
+    if (context instanceof FindsById) {
+      return ((FindsById) context).findElementsById(id);
+    }
+    return ((FindsByXPath) context).findElementsByXPath("*[@id = '" + id + "']");
+  }
+
+  @Override
+  public WebElement findElement(final SearchContext context) {
+    // Copy/paste of By.id with $id/page offset id interpolation added
+    final String id = getInterpolatedId();
+    if (context instanceof FindsById) {
+      return ((FindsById) context).findElementById(id);
+    }
+    return ((FindsByXPath) context).findElementByXPath("*[@id = '" + id + "']");
+  }
+
+  @Override
+  public String toString() {
+    return "By.id: " + getInterpolatedId();
+  }
+
+  private String getInterpolatedId() {
+    return page.getOffsetId() == null ? id : id.replace("$id", page.getOffsetId());
+  }
+}
