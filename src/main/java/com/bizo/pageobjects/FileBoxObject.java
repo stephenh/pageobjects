0ab63db129090a2b637cfba8e0471ceef5caa3f7
Stephen Haberman <stephen@exigencecorp.com>
2011-11-29 15:45:08 -0600

    Fix FileBoxObject to be generic and work in non-FF browsers.

--- src/main/java/com/bizo/pageobjects/FileBoxObject.java
+++ src/main/java/com/bizo/pageobjects/FileBoxObject.java
@@ -5,23 +5,30 @@ import org.openqa.selenium.JavascriptExecutor;
 
 public class FileBoxObject extends AbstractElementObject {
 
+  private final String id;
+
+  /** Only take an id for our firefox change hack. */
   public FileBoxObject(final PageObject p, final String id) {
     super(p, id);
-  }
-
-  public FileBoxObject(final PageObject p, final By by) {
-    super(p, by);
+    this.id = id;
   }
 
   public void type(final String value) {
     getElement().sendKeys(value);
     // Firefox does not fire onchange even if we send tab (it gets mangled) or
     // focus on another element, so do this the very explicit way
-    final String script = "var e = document.createEvent('HTMLEvents');"//
-      + "e.initEvent('change', true, true);"//
-      + "document.getElementsByClassName('gwt-FileUpload')[0].dispatchEvent(e);"//
-      + "return true;";
-    ((JavascriptExecutor) getWebDriver()).executeScript(script);
+    if (getWebDriver() instanceof JavascriptExecutor) {
+      final String script = "var element = document.getElementById('" + id + "');" //
+        + "if (element.dispatchEvent) {"
+        + "var e = document.createEvent('HTMLEvents');"
+        + "e.initEvent('change', true, true);"
+        + "element.dispatchEvent(e);"
+        + "}"
+        + "return true;";
+      ((JavascriptExecutor) getWebDriver()).executeScript(script);
+    }
+    // go ahead and click dummy-click-div in case its not FF
+    getWebDriver().findElement(By.id("dummy-click-div")).click();
   }
 
 }
