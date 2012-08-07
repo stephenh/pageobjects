debf1c1ebec95329d735042052b00bbcbf02894a
Stephen Haberman <stephen@exigencecorp.com>
2012-08-07 13:46:59 -0500

    The webdriver timeout is in seconds.

--- src/main/java/com/bizo/pageobjects/PageObjectSettings.java
+++ src/main/java/com/bizo/pageobjects/PageObjectSettings.java
@@ -4,14 +4,14 @@ import java.util.concurrent.atomic.AtomicLong;
 
 public class PageObjectSettings {
 
-  private static AtomicLong timeout = new AtomicLong(15000);
+  private static AtomicLong timeout = new AtomicLong(15);
 
   public static long getTimeout() {
     return timeout.get();
   }
 
-  public static void setTimeout(Long timeoutInMilliseconds) {
-    timeout.set(timeoutInMilliseconds);
+  public static void setTimeout(long timeoutInSeconds) {
+    timeout.set(timeoutInSeconds);
   }
     
 }
