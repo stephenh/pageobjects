9ce43477f2f71e5462917e749b4cafa9f02e4b40
Stephen Haberman <stephen@exigencecorp.com>
2013-06-20 14:29:58 -0500

    Add prefix and early-bind offset id replacement.
    This simplifies things as we can do the id replacement immediately in
    AbstractPageObject's element factory methods (text, textBox, etc.) and not worry
    about changing ByXxx classes to know about the prefix/offset.

--- src/main/java/com/bizo/pageobjects/AbstractPageObject.java
+++ src/main/java/com/bizo/pageobjects/AbstractPageObject.java
@@ -13,10 +13,21 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 public class AbstractPageObject implements PageObject {
 
   protected final WebDriver d;
-  protected String offsetId = null;
+  private final String prefix;
+  private final String offset;
 
   protected AbstractPageObject(final WebDriver d) {
+    this(d, null);
+  }
+
+  protected AbstractPageObject(final WebDriver d, final String prefix) {
+    this(d, prefix, null);
+  }
+
+  protected AbstractPageObject(final WebDriver d, final String prefix, final String offset) {
     this.d = d;
+    this.prefix = prefix;
+    this.offset = offset;
   }
 
   @Override
@@ -34,34 +45,44 @@ public class AbstractPageObject implements PageObject {
     }
   }
 
-  /** @return xpath interpolated with our offset id. */
-  protected By xpath(final String xpathExpression) {
-    return new ByXPathWithInterpolation(this, xpathExpression);
+  /** @return a CheckBoxObject for id. */
+  protected CheckBoxObject checkBox(final String id) {
+    return new CheckBoxObject(this, mungeId(id));
   }
 
-  /** @return a TextObject for id. */
-  protected TextObject text(final String id) {
-    return new TextObject(this, id);
+  /** @return a CheckBoxObject for by. */
+  protected CheckBoxObject checkBox(final By by) {
+    return new CheckBoxObject(this, by);
   }
 
-  /** @return a TextObject for by. */
-  protected TextObject text(final By by) {
-    return new TextObject(this, by);
+  /** @return an ElementObject for id. */
+  protected ElementObject element(final String id) {
+    return new ElementObject(this, mungeId(id));
   }
 
-  /** @return a TextBoxObject for id. */
-  protected TextBoxObject textBox(final String id) {
-    return new TextBoxObject(this, id);
+  /** @return an ElementObject for by. */
+  protected ElementObject element(final By by) {
+    return new ElementObject(this, by);
   }
 
-  /** @return a TextBoxObject for by. */
-  protected TextBoxObject textBox(final By by) {
-    return new TextBoxObject(this, by);
+  /** @return a FileBoxObject for id. */
+  protected FileBoxObject file(final String id) {
+    return new FileBoxObject(this, mungeId(id));
+  }
+
+  /** @return an ImageObject for id. */
+  protected ImageObject image(final String id) {
+    return new ImageObject(this, mungeId(id));
+  }
+
+  /** @return an ImageObject for by. */
+  protected ImageObject image(final By by) {
+    return new ImageObject(this, by);
   }
 
   /** @return a LinkObject for id. */
   protected LinkObject link(final String id) {
-    return new LinkObject(this, id);
+    return new LinkObject(this, mungeId(id));
   }
 
   /** @return a LinkObject for by. */
@@ -69,14 +90,54 @@ public class AbstractPageObject implements PageObject {
     return new LinkObject(this, by);
   }
 
-  @Override
-  public WebDriver getWebDriver() {
-    return d;
+  /** @return a ListObject for id. */
+  protected ListObject list(final String id) {
+    return new ListObject(this, mungeId(id));
+  }
+
+  /** @return a ListObject for by. */
+  protected ListObject list(final By by) {
+    return new ListObject(this, by);
+  }
+
+  /** @return a SelectObject for id. */
+  protected SelectObject select(final String id) {
+    return new SelectObject(this, mungeId(id));
+  }
+
+  /** @return a SelectObject for by. */
+  protected SelectObject select(final By by) {
+    return new SelectObject(this, by);
+  }
+
+  /** @return a TextBoxObject for id. */
+  protected TextBoxObject textBox(final String id) {
+    return new TextBoxObject(this, mungeId(id));
+  }
+
+  /** @return a TextBoxObject for by. */
+  protected TextBoxObject textBox(final By by) {
+    return new TextBoxObject(this, by);
+  }
+  /** @return a TextObject for id. */
+  protected TextObject text(final String id) {
+    return new TextObject(this, mungeId(id));
+  }
+
+  /** @return a TextObject for by. */
+  protected TextObject text(final By by) {
+    return new TextObject(this, by);
+  }
+
+
+  protected String mungeId(String id) {
+    return //
+    (prefix == null ? "" : prefix) + (offset == null ? id : id.replace("$id", offset));
   }
 
   @Override
-  public String getOffsetId() {
-    return offsetId;
+  public WebDriver getWebDriver() {
+    return d;
   }
 
 }
--- src/main/java/com/bizo/pageobjects/PageObject.java
+++ src/main/java/com/bizo/pageobjects/PageObject.java
@@ -10,9 +10,6 @@ public interface PageObject {
   /** @return the {@link WebDriver} instance for this page object */
   WebDriver getWebDriver();
 
-  /** @return the string to interpolate into string-based elements ids */
-  String getOffsetId();
-
   /** Wait for {@code conditions} to be true before continuing */
   void waitFor(ExpectedCondition<?>... conditions);
 
